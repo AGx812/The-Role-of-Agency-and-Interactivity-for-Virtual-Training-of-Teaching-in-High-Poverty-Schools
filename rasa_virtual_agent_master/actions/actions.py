4c125d21874105ed01b19d24d5f5cf1a1c3fb28b
Abdullah Ghanima <abdullah.ghanima@gmail.com>
2021-11-20 08:20:33 -0500

    Add files via upload

---
+++ rasa_virtual_agent_master/actions/actions.py
@@ -0,0 +1,49 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+# class ActionHelloWorld(Action):
+
+#     def name(self) -> Text:
+#         return "action_hello_world"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(text="Hello World!")
+
+#         return []
+
+# class ActionPrintIntent(Action):
+#     def name(self) -> Text:
+#         return "action_print_intent"
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         # dispatcher.utter_message(text='Your intent is '+tracker.latest_message['intent'].get('name'))
+
+#         return [tracker.latest_message['intent'].get('name')] 
+
+# class ActionSetSlot(Action):
+#     def name(self) -> Text:
+#         return "action_set_slot"
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         # dispatcher.utter_message(text='Your intent is '+tracker.latest_message['intent'].get('name'))
+
+#         return [tracker.latest_message['intent'].get('name')] 
